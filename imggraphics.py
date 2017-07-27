47fc158573f9f0f5d47f90c411327ed97f456598
happysmash27 <happysmash27@vmail.me>
2017-07-27 09:33:54 -0700

    Added a framebuffer/image backend, as the current backend doesn't work

---
+++ imggraphics.py
@@ -0,0 +1,33 @@
+import matplotlib
+matplotlib.use('TkAgg')
+
+import numpy as np
+import matplotlib as mpl
+mpl.use("Agg")
+import matplotlib.pyplot as plt
+#import plotly.plotly as py
+#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+# implement the default mpl key bindings
+from matplotlib.backend_bases import key_press_handler
+import mpl_scatter_density
+
+
+from matplotlib.figure import Figure
+
+import sys
+
+import subprocess
+
+def plotAsteroids(x,y):
+
+    #f = Figure(figsize=(5, 4), dpi=100)
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1, projection='scatter_density')
+
+    #a.plot(x,y,"o")
+    ax.scatter_density(x,y)
+    ax.set_xlim(-5, 10)
+    ax.set_ylim(-5, 10)
+
+    plt.savefig("imggraphics.png")
+    subprocess.call(["fbi","imggraphics.png"])
